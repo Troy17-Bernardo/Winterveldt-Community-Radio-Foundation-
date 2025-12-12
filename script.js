372eb75b1d97d89d69af032817b70dbcf0bb4f33
Troy Bernardo <troybernardo248@gmail.com>
2025-12-12 23:35:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.addEventListener('DOMContentLoaded',()=>{});
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.management-photo').forEach(photo => {
+    photo.addEventListener('mousedown', () => {
+      photo.style.transform = 'scale(1.05)';
+    });
+
+    photo.addEventListener('mouseup', () => {
+      photo.style.transform = '';
+    });
+
+    photo.addEventListener('mouseleave', () => {
+      photo.style.transform = '';
+    });
+  });
+});
